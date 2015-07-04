262ac7e6cc0acce12127c7bc7508839365aaf316
Tara <tararmassey@yahoo.com>
2015-07-03 20:00:00 -0700

    dropdown for lower right box added

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+/****************************************************
+*			Horizontal slider
+*****************************************************/
 /* basic horizontal slider, used for reading more info in side boxes */
+
+/* Upper right box */
 $(document).ready(function(){
     $('.flip').click(function(){
         $('.panel').slideDown('slow');		/* more */
     });
     $('.panel').click(function(){
         $('.panel').slideUp('slow');		/* hide/less */
+    });
+
+/* lower right box */
+    $('.flip2').click(function(){
+        $('.panel2').slideDown('slow');		/* more */
+    });
+    $('.panel2').click(function(){
+        $('.panel2').slideUp('slow');		/* hide/less */
     });
 });
