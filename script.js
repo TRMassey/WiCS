e33f4d01d7b943f8787e91f5c0068cc28281ab6e
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-08-14 20:04:36 -0500

    Mobile menu added

--- script.js
+++ script.js
@@ -43,3 +43,45 @@ $(document).ready(function() {
         }
     });
 });
+
+/****************************************************
+*             Mobile Friendly Menu
+*****************************************************/
+
+$(document).ready(function () {
+    var w = $(window).width();
+
+    if (w < 601) {
+        // hide the oregon state text
+        jQuery('h1:first').hide();
+
+        // hide the extraneous columns to keep formatting
+        jQuery('.rightMain').hide();
+
+        jQuery('.rightMainSecond').hide();
+    }
+});
+
+// undo the above if the window is expanded
+$(window).on('resize', function() {
+    var win = $(this);
+
+    if (win.width() > 760) {
+        jQuery('h1:first').show();
+
+        jQuery('.rightMain').show();
+
+        jQuery('.rightMainSecond').show();
+    }
+
+    // does the same as above to handle the window being manually resized
+    if (win.width() < 760) {
+        // hide the oregon state text
+        jQuery('h1:first').hide();
+
+        // hide the extraneous columns to keep formatting
+        jQuery('.rightMain').hide();
+
+        jQuery('.rightMainSecond').hide();
+    }
+});
